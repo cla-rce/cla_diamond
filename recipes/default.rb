6e98db2f67a6bc2fea48d708d91b891b4efafff6
Andrew Boldenow <bolde014@umn.edu>
2013-11-12 17:52:58 -0600

    updated attributes list

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-node.override["diamond"]["install_method"] = "source"
+node.override["diamond"]["install_method"] = "package"
 node.override["diamond"]["graphite_server_role"] = nil
 node.override["diamond"]["graphite_server"] = "erato.cla.umn.edu"
 node.override["diamond"]["path_prefix"] = "servers"
@@ -18,6 +18,8 @@ node.override["diamond"]["source_path"] = "/usr/local/share/diamond_src"
 #node.override["diamond"]["source_reference"] = '3.4'
 node.override['diamond']['add_collectors'] = ['cpu', 'diskspace', 'diskusage', 'loadavg', 'memory', 'network', 'vmstat', 'tcp']
 node.override[:diamond][:collectors][:DiskSpaceCollector][:filesystems] = 'ext2, ext3, ext4'
+node.override["diamond"]["cookbook_package"]["debian"] = "diamond_3.4.deb"
+node.override["diamond"]["cookbook_package"]["redhat"] = "diamond_3.4.noarch.rpm"
 
 
 include_recipe "diamond"
