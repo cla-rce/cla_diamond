a52eb63e5db2c27bc844ddaee6dab014eb7b8c8f
Andrew Boldenow <bolde014@umn.edu>
2013-12-05 13:41:25 -0600

    changed attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,13 @@
 #
 # Cookbook Name:: cla_diamond
-# Recipe:: node.override
+# Recipe:: default
 #
 # Copyright (C) 2013 YOUR_NAME
 # 
 # All rights reserved - Do Not Redistribute
 #
 
-node.override["diamond"]["install_method"] = "package"
+node.override["diamond"]["install_method"] = "source"
 node.override["diamond"]["graphite_server_role"] = nil
 node.override["diamond"]["graphite_server"] = "erato.cla.umn.edu"
 node.override["diamond"]["path_prefix"] = "servers"
@@ -15,7 +15,7 @@ node.override["diamond"]["interval"] = "100"
 node.override["diamond"]["handlers"] = "diamond.handler.graphite.GraphiteHandler, diamond.handler.archive.ArchiveHandler"
 node.override["diamond"]["source_repository"] = "git://github.com/BrightcoveOS/Diamond.git"
 node.override["diamond"]["source_path"] = "/usr/local/share/diamond_src"
-#node.override["diamond"]["source_reference"] = '3.4'
+node.override["diamond"]["source_reference"] = 'v3.4'
 node.override['diamond']['add_collectors'] = ['cpu', 'diskspace', 'diskusage', 'loadavg', 'memory', 'network', 'vmstat', 'tcp']
 node.override[:diamond][:collectors][:DiskSpaceCollector][:filesystems] = 'ext2, ext3, ext4'
 node.override["diamond"]["cookbook_package"]["debian"] = "diamond_3.4.deb"
